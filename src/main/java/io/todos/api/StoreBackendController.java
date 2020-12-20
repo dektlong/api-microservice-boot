1f13ce7553c109e2ba53d9fc087b0c0c0e746084
dektlong <dekel.tankel@gmail.com>
2020-12-20 11:15:58 -0800

    Update and rename TodosController.java to StoreBackendController.java

--- src/main/java/io/todos/api/StoreBackendController.java
+++ src/main/java/io/todos/api/StoreBackendController.java
@@ -1,4 +1,4 @@
-package io.todos.api;
+package io.store-backend.api;
 
 import io.todos.api.data.Todo;
 import org.slf4j.Logger;
@@ -16,11 +16,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @RestController
-public class TodosController {
+public class StoreBackendController {
 
     private static Logger LOG = LoggerFactory.getLogger(TodosController.class);
 
-    @Value("${todos.api.limit:100}")
+    @Value("${store-backend.api.limit:100}")
     long _limit;
 
     //@Value("${cacheUrl:http://localhost:8888}")
@@ -37,9 +37,9 @@ public class TodosController {
     private static String DEFAULT_GROUP = "Default group";
 
     @GetMapping("/")
-    public List<Todo> retrieve() {
-        LOG.debug("Retrieving all Todos");
-        Todo[] cached = _cacheTemplate.getForEntity(_cacheUrl, Todo[].class).getBody();
+    public List<StoreObject> retrieve() {
+        LOG.debug("Retrieving all StoreObjects");
+        StoreObject[] cached = _cacheTemplate.getForEntity(_cacheUrl, StoreObject[].class).getBody();
         //if cache is empty, hydrate
         if(cached.length < 1) {
             LOG.debug("Cache empty, retrieving from backend service");
@@ -55,88 +55,88 @@ public class TodosController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/")
-    public Todo create(@RequestBody Todo todo) {
+    public StoreObject create(@RequestBody StoreObject storeObject) {
         // check if cache size is not over the limit
         throwIfOverLimit();
 
-        if(todo.getTitle() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todos.title cannot be null on put");
+        if(storeObject.getTitle() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "storeObject.title cannot be null on put");
         }
 
-        LOG.debug("Creating TODO: " + todo);
-        Todo obj = new Todo();
-        if(ObjectUtils.isEmpty(todo.getId())) {
+        LOG.debug("Creating TODO: " + storeObject);
+        StoreObject obj = new StoreObject();
+        if(ObjectUtils.isEmpty(storeObject.getId())) {
             obj.setId(UUID.randomUUID().toString());
         } else {
-            obj.setId(todo.getId());
+            obj.setId(storeObject.getId());
         }
-        if(!ObjectUtils.isEmpty(todo.getTitle())) {
+        if(!ObjectUtils.isEmpty(storeObject.getTitle())) {
             obj.setTitle(todo.getTitle());
         }
         if(!ObjectUtils.isEmpty(todo.isComplete())) {
             obj.setComplete(todo.isComplete());
         }
-        if(ObjectUtils.isEmpty(todo.getCategory())) {
+        if(ObjectUtils.isEmpty(storeObject.getCategory())) {
             obj.setCategory(DEFAULT_GROUP);
         } else {
-            obj.setCategory(todo.getCategory());
+            obj.setCategory(storeObject.getCategory());
         }
-        if(ObjectUtils.isEmpty(todo.getDeadline())) {
+        if(ObjectUtils.isEmpty(storeObject.getDeadline())) {
             obj.setDeadline(dtf.format(LocalDateTime.now()));
         } else {
-            obj.setDeadline(todo.getDeadline());
+            obj.setDeadline(storeObject.getDeadline());
         }
 
         //Write to DB
-        Todo saved = _backendTemplate.postForObject(_backendUrl, obj, Todo.class);
+        StoreObject saved = _backendTemplate.postForObject(_backendUrl, obj, StoreObject.class);
         LOG.debug("Created in Backend");
 
         //Invalidate/Add Cache
-        Todo cached = _cacheTemplate.postForObject(_cacheUrl, saved, Todo.class);
+        StoreObject cached = _cacheTemplate.postForObject(_cacheUrl, saved, StoreObject.class);
         LOG.debug("Created in Cache");
         return saved;
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/{id}")
-    public Todo put(@PathVariable String id, @RequestBody Todo todo) {
-        if(todo.getId() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todos.id cannot be null on put");
+    public StoreObject put(@PathVariable String id, @RequestBody StoreObject storeObject) {
+        if(storeObject.getId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "storeObject.id cannot be null on put");
         }
-        if(!todo.getId().equals(id)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todos.id ${todo.id} and id $id are inconsistent");
+        if(!storeObject.getId().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "storeObject.id ${storeObject.id} and id $id are inconsistent");
         }
 
-        Todo obj = new Todo();
-        obj.setId(todo.getId());
+        StoreObject obj = new StoreObject();
+        obj.setId(storeObject.getId());
 
-        if(!ObjectUtils.isEmpty(todo.isComplete())) {
-            obj.setComplete(todo.isComplete());
+        if(!ObjectUtils.isEmpty(storeObject.isComplete())) {
+            obj.setComplete(storeObject.isComplete());
         }
-        if(ObjectUtils.isEmpty(todo.getCategory())) {
+        if(ObjectUtils.isEmpty(storeObject.getCategory())) {
             obj.setCategory(DEFAULT_GROUP);
         } else {
             obj.setCategory(todo.getCategory());
         }
-        if(ObjectUtils.isEmpty(todo.getDeadline())) {
+        if(ObjectUtils.isEmpty(storeObject.getDeadline())) {
             obj.setDeadline(dtf.format(LocalDateTime.now()));
         } else {
-            obj.setDeadline(todo.getDeadline());
+            obj.setDeadline(storeObject.getDeadline());
         }
 
         //Write to DB
-        Todo saved = _backendTemplate.postForObject(_backendUrl, obj, Todo.class);
+        StoreObject saved = _backendTemplate.postForObject(_backendUrl, obj, StoreObject.class);
         LOG.debug("Created in Backend");
 
         //Invalidate/Add Cache
-        Todo cached = _cacheTemplate.postForObject(_cacheUrl, saved, Todo.class);
+        StoreObject cached = _cacheTemplate.postForObject(_cacheUrl, saved, StoreObject.class);
         LOG.debug("Created in Cache");
         return saved;
     }
 
     @DeleteMapping("/")
     public void deleteAll() {
-        LOG.debug("Removing all Todos");
+        LOG.debug("Removing all StoreObjects");
 
         //Remove from DB
         _backendTemplate.delete(_backendUrl);
@@ -145,12 +145,12 @@ public class TodosController {
     }
 
     @GetMapping("/{id}")
-    public Todo retrieve(@PathVariable("id") String id) {
-        LOG.debug("Retrieving Todo: " + id);
+    public StoreObject retrieve(@PathVariable("id") String id) {
+        LOG.debug("Retrieving StoreObject: " + id);
         //Check cache + DB
         Todo cached = null;
         try {
-            cached = _cacheTemplate.getForEntity(_cacheUrl + "/" + id, Todo.class).getBody();
+            cached = _cacheTemplate.getForEntity(_cacheUrl + "/" + id, StoreObject.class).getBody();
         } catch (HttpStatusCodeException ex) {
             if(ex.getRawStatusCode() != 404) {
                 LOG.error("Caching service error downstream", ex);
@@ -167,7 +167,7 @@ public class TodosController {
 
             Todo source = null;
             try{
-                source = _backendTemplate.getForEntity(_backendUrl + "/" + id, Todo.class).getBody();
+                source = _backendTemplate.getForEntity(_backendUrl + "/" + id, StoreObject.class).getBody();
             } catch (HttpStatusCodeException ex) {
                 if(ex.getRawStatusCode() != 404) {
                     LOG.error("Database service error downstream", ex);
@@ -177,12 +177,12 @@ public class TodosController {
 
             if(source != null) {
                 LOG.debug("Found in backend");
-                cached = _cacheTemplate.postForObject(_cacheUrl, source, Todo.class);
+                cached = _cacheTemplate.postForObject(_cacheUrl, source, StoreObject.class);
                 return source;
             }
         }
 
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "todo.id = " + id);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "storeObject.id = " + id);
     }
 
     @DeleteMapping("/{id}")
@@ -195,48 +195,48 @@ public class TodosController {
     }
 
     @PatchMapping("/{id}")
-    public Todo update(@PathVariable("id") String id, @RequestBody Todo todo) {
-        if(todo.getId() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todos.id cannot be null on put");
+    public StoreObject update(@PathVariable("id") String id, @RequestBody StoreObject storeObject) {
+        if(storeObject.getId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "storeObject.id cannot be null on put");
         }
-        if(!todo.getId().equals(id)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todos.id ${todo.id} and id $id are inconsistent");
+        if(!storeObject.getId().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "storeObject.id ${todo.id} and id $id are inconsistent");
         }
 
-        Todo obj = new Todo();
-        obj.setId(todo.getId());
+        StoreObject obj = new StoreObject();
+        obj.setId(storeObject.getId());
 
-        if(!ObjectUtils.isEmpty(todo.getTitle())) {
-            obj.setTitle(todo.getTitle());
+        if(!ObjectUtils.isEmpty(storeObject.getTitle())) {
+            obj.setTitle(storeObject.getTitle());
         }
-        if(!ObjectUtils.isEmpty(todo.isComplete())) {
-            obj.setComplete(todo.isComplete());
+        if(!ObjectUtils.isEmpty(storeObject.isComplete())) {
+            obj.setComplete(storeObject.isComplete());
         }
-        if(ObjectUtils.isEmpty(todo.getCategory())) {
+        if(ObjectUtils.isEmpty(storeObject.getCategory())) {
             obj.setCategory(DEFAULT_GROUP);
         } else {
-            obj.setCategory(todo.getCategory());
+            obj.setCategory(storeObject.getCategory());
         }
-        if(ObjectUtils.isEmpty(todo.getDeadline())) {
+        if(ObjectUtils.isEmpty(storeObject.getDeadline())) {
             obj.setDeadline(dtf.format(LocalDateTime.now()));
         } else {
-            obj.setDeadline(todo.getDeadline());
+            obj.setDeadline(storeObject.getDeadline());
         }
 
         //Write to DB
-        Todo saved = _backendTemplate.postForObject(_backendUrl, obj, Todo.class);
+        StoreObject saved = _backendTemplate.postForObject(_backendUrl, obj, StoreObject.class);
         LOG.debug("Created in Backend");
 
         //Invalidate/Add Cache
-        Todo cached = _cacheTemplate.postForObject(_cacheUrl, saved, Todo.class);
+        StoreObject cached = _cacheTemplate.postForObject(_cacheUrl, saved, StoreObject.class);
         LOG.debug("Created in Cache");
         return saved;
     }
 
     private void throwIfOverLimit() {
-        Todo[] cached = _cacheTemplate.getForEntity(_cacheUrl, Todo[].class).getBody();
+        StoreObject[] cached = _cacheTemplate.getForEntity(_cacheUrl, Todo[].class).getBody();
         if(cached.length >= _limit) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "todos.api.limit=$limit, todos.size=$count");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "storeObject.api.limit=$limit, storeObject.size=$count");
         } else {
             return;
         }
