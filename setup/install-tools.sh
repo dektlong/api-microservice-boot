490e71119819905762a182a55d4ef30914c5f78a
dektlong <dekel.tankel@gmail.com>
2021-03-08 13:39:28 -0800

    Delete install-tools.sh

--- setup/install-tools.sh
+++
@@ -1,4 +0,0 @@
-# Using Bitnami
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install todo-mysql-instance bitnami/mariadb --set rootUser.password=topsecret
-helm install todo-redis-instance bitnami/redis --set global.redis.password=topsecret
