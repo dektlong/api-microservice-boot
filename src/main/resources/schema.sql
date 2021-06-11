feaf03d4d973f5cf14c3adfe00b87aadaef8d2ac
dektlong <dekel.tankel@gmail.com>
2021-06-11 09:46:45 -0700

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,7 @@
-create table animal (
+create table store-item (
   id bigint primary key auto_increment,
-  name varchar(255) not null,
-  rescue_date date,
-  avatar_url varchar(255),
-  description varchar(1000)
+  title varchar(255) not null,
+  category varchar(1000)
 );
 
 create table adoption_request (
