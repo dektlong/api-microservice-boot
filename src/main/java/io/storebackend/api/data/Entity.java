95cadaca83083ca77d9e3da841d6fd00b37a4e0e
dekt <dekel@vmware.com>
2021-08-19 10:26:58 -0700

    here goes nothing

---
+++ src/main/java/io/storebackend/api/data/Entity.java
@@ -0,0 +1,8 @@
+package io.storebackend.api.data;
+
+public class EntityName {
+
+    private String id; //TODO add the detaills of your EntityName entity
+
+    //implement more here and document
+}
