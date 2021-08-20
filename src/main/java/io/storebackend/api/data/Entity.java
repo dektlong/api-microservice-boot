0fd05c3ea5dcc97bdc7ee47dcf5418f6c57da1df
dektlong <dekel.tankel@gmail.com>
2021-08-20 01:49:44 -0700

    Update and rename Entity.java to MongoTemplate.java

--- src/main/java/io/storebackend/api/data/Entity.java
+++
@@ -1,8 +0,0 @@
-package io.storebackend.api.data;
-
-public class entityNameEntity {
-
-    private String id; //TODO add the detaills of your entityName entity
-
-    //implement more here and document
-}
---
+++ src/main/java/io/storebackend/api/data/MongoTemplate.java
@@ -0,0 +1,18 @@
+package io.storebackend.api.data;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+@Profile("mongodb")
+public class MongoTemplate {
+
+    @Bean("bean-name")
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory) {
+        return new MongoTemplate(mongoDbFactory);
+    }
+
+}
