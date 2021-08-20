fb1bef1f791513a25a9014742ecc013b619a4485
dektlong <dekel.tankel@gmail.com>
2021-08-20 01:55:34 -0700

    Create PostresTemplate.java

---
+++ src/main/java/io/storebackend/api/data/PostresTemplate.java
@@ -0,0 +1,19 @@
+package io.storebackend.api.data;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("postgres")
+public class PostgresTemplate extends AbstractCloudConfig {
+
+    @Bean("mybean")
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+
+}
