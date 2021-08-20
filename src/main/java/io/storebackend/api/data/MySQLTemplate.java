8c203ad82e718eb205300401c4731d372a8070fb
dektlong <dekel.tankel@gmail.com>
2021-08-20 01:54:19 -0700

    Create MySQLTemplate.java

---
+++ src/main/java/io/storebackend/api/data/MySQLTemplate.java
@@ -0,0 +1,19 @@
+package io.storebackend.api.data;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("mysql")
+public class MySQLTemplate extends AbstractCloudConfig {
+
+    @Bean("mybean")
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+
+}
